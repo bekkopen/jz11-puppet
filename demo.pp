e9cb1b0c30904d5c3066c34e2993c50c3d1f4c5e
Ole Christian Rynning <oc@rynning.no>
2011-08-25 22:12:09 +0200

    Changed host

--- demo.pp
+++ demo.pp
@@ -7,11 +7,15 @@ file { $www_root:
 }
 
 nginx::site { "jz11":
-  domain => "jz11.uggedal.com",
+  domain => "jz11.muda.no",
   root => $www_root,
   upstreams => ["localhost:8080"],
 }
 
 Class["nginx"] -> File[$www_root] -> Nginx::Site["jz11"]
+
+# Install Java
+#package { "openjdk-6-jre":
+#}
 
 # TODO: install and configure Jetty
