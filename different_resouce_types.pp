6e663b0963a6f426d1c5606df00a40d42c9014b2
Eivind Uggedal <eivind@uggedal.com>
2011-09-04 15:48:53 +0200

    Manage home dir for oc user.

--- different_resouce_types.pp
+++ different_resouce_types.pp
@@ -1,8 +1,9 @@
 user { "oc":
-  ensure => present,
-  uid    => 1337,
-  home   => "/home/oc",
-  password => '$6$xSZZx34H$7Wv2cIRg/MP2BGGp64OOmjxL6vxfl2QxNsmsRylxWsyA4maIp20lot3rw76Bl75DXfqltqDQUwNNWmHSTlEuJ/',
+  ensure     => present,
+  uid        => 1337,
+  home       => "/home/oc",
+  managehome => "/home/oc",
+  password   => '$6$xSZZx34H$7Wv2cIRg/MP2BGGp64OOmjxL6vxfl2QxNsmsRylxWsyA4maIp20lot3rw76Bl75DXfqltqDQUwNNWmHSTlEuJ/',
 }
 
 cron { "poor-mans-http-ping":
