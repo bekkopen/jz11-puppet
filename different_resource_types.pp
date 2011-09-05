39904b3c25e3aa8e79201e1534b7f4de90b1b45f
Ole Christian Rynning <oc@rynning.no>
2011-09-05 21:35:24 +0200

    fix

--- different_resource_types.pp
+++ different_resource_types.pp
@@ -6,14 +6,7 @@ user { "oc":
   password   => '$6$xSZZx34H$7Wv2cIRg/MP2BGGp64OOmjxL6vxfl2QxNsmsRylxWsyA4maIp20lot3rw76Bl75DXfqltqDQUwNNWmHSTlEuJ/',
 }
 
-ssh_authorized_key { "oc@snappy":
-    ensure  => present,
-    key => "AAAAB3NzaC1yc2EAAAABIwAAAQEAmZWL6WJML2kP4deIhCq/YY4DU+SXmXkpLgVvQMLJ+qggo6fXnG+bre/K7LOyC8nowl3qkBhFD/wq0c8LLlAqEeZ9o8lrwNIHck3dOtkri72UCm2CGWdLX4N36eI3wThSdbdiKVVpK9KRYjVqgs6f75raQ4NaDb9fc4j5sffDycYpJYibhu1ZcSL6TrnJQKVHUKysT0h9XnrR+/QaAs7fXG3LC+dMpj4dWQwa5nva5K93yyLlBY3fql5FeEVoVOmDz4dE5hPD+HtM9HIFWBf+Q644qjh9gtX4VEOQnk7y6vkn+gr+egwnJRe7Lse7J2Bb1vNednWFrBFBp0zPium5OQ==",
-    user  => oc,
-    type  => "ssh-rsa",
-}
-
 cron { "poor-mans-http-ping":
-  command => "curl -s http://demo.muda.no/ > /dev/null 2>&1",
+  command => "curl -s http://wasitup.com/ > /dev/null 2>&1",
   minute  => "*/1", # Sjekk hvert minutt
 }
