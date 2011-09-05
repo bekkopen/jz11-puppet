8113ef7c4d9b35372812d71effccd4b4c487a855
Eivind Uggedal <eivind@uggedal.com>
2011-09-05 19:10:51 +0200

    moo

--- init/standalone.sh
+++ init/standalone.sh
@@ -2,8 +2,13 @@
 #
 # Initialize a new puppet standalone machine
 #
-# export HOSTNAME=demo.muda.no && bash < <(curl -s https://raw.github.com/bekkopen/jz11-puppet/master/init/standalone.sh)
+# bash < <(curl -s https://raw.github.com/bekkopen/jz11-puppet/master/init/standalone.sh)
 #
+
+if [[ -z $NEWHOSTNAME ]]; then
+  echo "ERROR: You need to export NEWHOSTNAME=desired.fqdn.host"
+  exit 1
+fi
 
 SQUEEZE_REPO="deb http://ukdebian.mirror.anlx.net/debian/ squeeze main contrib"
 
@@ -16,11 +21,9 @@ EOF
 
 PUBLIC_IP_ADDRESS=`ifconfig eth0 | awk -F':' '/inet addr/{split($2,_," ");print _[1]}'`
 
-echo $HOSTNAME > /etc/hostname && hostname -F /etc/hostname
+echo $NEWHOSTNAME > /etc/hostname && hostname -F /etc/hostname
 echo $PUBLIC_IP_ADDRESS `hostname` `hostname -s` >> /etc/hosts
 
 apt-get -qq update && apt-get -qq -y install puppet git-core
 
-git clone https://github.com/bekkopen/jz11-puppet /root/jz-puppet
-cd /root/jz-puppet
-git submodule update --init
+git clone https://github.com/bekkopen/jz11-puppet /root/jz-puppet && cd /root/jz-puppet && git submodule update --init
